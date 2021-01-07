178d2d49ebf60b8d59e7274e6164e5dd33f19b97
Arian Pasquali <arianpasquali@gmail.com>
2021-01-07 21:33:56 +0000

    experiments script

---
+++ run_experiments.sh
@@ -0,0 +1,12 @@
+#bin/predict-timelines configs/tlscovid19/tlsconstraints_upper_bound_covid19_en.json > tlsconstraints_upper_bound_covid19_en.txt
+#bin/predict-timelines configs/tlscovid19/asmds_upper_bound_covid19_en.json > asmds_upper_bound_covid19_en.txt
+#bin/predict-timelines configs/tlscovid19/random_covid19_en.json > random_covid19_en.txt
+#bin/predict-timelines configs/tlscovid19/asmds_dateref_covid19_en.json > asmds_dateref_covid19_en.txt
+bin/predict-timelines configs/tlscovid19/tlsconstraints_reweighting_dateref_covid19_en.json > tlsconstraints_reweighting_dateref_covid19_en.txt
+
+
+# bin/predict-timelines configs/tlscovid19/tlsconstraints_upper_bound_covid19_pt.json > tlsconstraints_upper_bound_covid19_pt.txt
+# bin/predict-timelines configs/tlscovid19/asmds_upper_bound_covid19_pt.json > asmds_upper_bound_covid19_pt.txt
+# bin/predict-timelines configs/tlscovid19/random_covid19_pt.json > random_covid19_pt.txt
+# bin/predict-timelines configs/tlscovid19/asmds_dateref_covid19_pt.json > asmds_dateref_covid19_pt.txt
+# bin/predict-timelines configs/tlscovid19/tlsconstraints_reweighting_dateref_covid19_pt.json > tlsconstraints_reweighting_dateref_covid19_pt.txt
