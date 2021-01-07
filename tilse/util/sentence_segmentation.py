6cc3f4da061ddca2bc5f465733e9a0efd5dad43e
Arian Pasquali <arianpasquali@gmail.com>
2021-01-07 23:14:18 +0100

    support ternip timeml preprocessing

--- tilse/util/sentence_segmentation.py
+++ tilse/util/sentence_segmentation.py
@@ -10,10 +10,10 @@ def sentence_segmenter(document):
                 # except for hyphenated word-breaks
                 # and replacing "n't" contractions with "not",
                 # separating tokens by single spaces
-                print(token.value, end=' ')
+                # print(token.value, end=' ')
                 s_sentence += token.value + " "
-            print()  # print one sentence per line
+            # print()  # print one sentence per line
             sentences.append(s_sentence)
-        print()  # separate paragraphs with newlines
+        # print()  # separate paragraphs with newlines
 
     return "\n".join(sentences)
