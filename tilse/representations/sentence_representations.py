385fa32e0123cf0247aff04a63b819560e6386cb
Arian Pasquali <arianpasquali@gmail.com>
2021-01-17 01:33:32 +0100

    fix evaluate models script

--- tilse/representations/sentence_representations.py
+++ tilse/representations/sentence_representations.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 
 import nltk
 import numpy
+
 from sklearn import metrics
 
 from tilse.util.constants import STOPWORDS
@@ -50,7 +51,7 @@ class SentenceRepresentation:
                 sents_vec.append(self._get_sentence_vector(sent))
 
         # return sents, numpy.array(sents_vec)
-        return sents, numpy.array(sents_vec).astype(np.float32)
+        return sents, numpy.array(sents_vec).astype(numpy.float32)
 
     def compute_pairwise_similarities(self):
         """
